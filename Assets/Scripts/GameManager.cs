1f6b8726253c900f099e269e6835c5aa0c2279aa
Alif Ahsan Nibir <ahsan2649@outlook.com>
2023-01-15 23:26:51 +0600

    Gamemanager started

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,6 +8,12 @@ public class GameManager : MonoBehaviour
     [SerializeField] FMODUnity.EventReference song;
     List<Character> characters= new List<Character>();
     List<Gun> guns= new List<Gun>();
+
+    private void Awake()
+    {
+        characters = FindObjectsOfType<Character>().ToList();
+        guns = FindObjectsOfType<Gun>().ToList();
+    }
 
     // Start is called before the first frame update
     void Start()
