970785f318a0d980de7ad9cebbc71b00ad250b08
Leon <leon@nyoucomer.de>
2023-02-01 03:57:54 +0100

    Bugfiix for crown and tooltip overlay

--- Assets/Scripts/Overhauled/PlayerManager.cs
+++ Assets/Scripts/Overhauled/PlayerManager.cs
@@ -316,7 +316,7 @@ public class PlayerManager : MonoBehaviour
                 crownHealth = player.health;
                 SoundManager.instance.PlayOneShot(FMODEvents.instance.crown, transform.position);
             }
-            else if (crownHealth == player.health)
+            else if (crownHealth == player.health && crownPlayer != player)
             {
                 if (crownPlayer != null)
                 {
